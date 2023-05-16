f5cdd3dc1e47b9dbd2f5a1a5d17a6d267844f368
peterAdedokun1 <peteradedokun436@gmail.com>
2023-05-16 10:44:48 +0100

    password reset

--- src/app/components/Modal/Auth/ForgotPassword.tsx
+++ src/app/components/Modal/Auth/ForgotPassword.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   Box,
   Button,
@@ -12,8 +12,14 @@ import { Formik, useFormik } from "formik";
 import { CheckIcon } from "@chakra-ui/icons";
 import { AuthModalState } from "@/atoms/AuthModalAtom";
 import { useSetRecoilState } from "recoil";
+import { useSendPasswordResetEmail } from 'react-firebase-hooks/auth';
+import { auth } from '@/firebase/clientApp';
 const ForgotPassword = () => {
-    const setAuthModalState = useSetRecoilState(AuthModalState);
+  const setAuthModalState = useSetRecoilState(AuthModalState);
+  const [success, setSuccess] = useState(false)
+    const [sendPasswordResetEmail, sending, error] =
+    useSendPasswordResetEmail(auth);
+  
     const validateSchema = yup.object().shape({
       email: yup
         .string()
@@ -22,7 +28,7 @@ const ForgotPassword = () => {
       username: yup
         .string()
         .min(3)
-        .max(20)
+ 
         .required("Username must be between 3 and 20 characters"),
     });
     const {
@@ -45,129 +51,148 @@ const ForgotPassword = () => {
       },
       validationSchema: validateSchema,
     });
+  
+  const onReset = async (e: React.FormEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    await sendPasswordResetEmail(values.username);
+    setSuccess(true);
+  };
   return (
     <Stack>
       <Text fontSize={"20px"} mb="5px" fontWeight={600}>
         Recover your password
       </Text>
-      <Text fontSize={"13px"} mb="20px">
-        Tell us the username and email address associated with your Reddit
-        account, and we’ll send you an email with a link to reset your password.
-      </Text>
-      <form onSubmit={handleSubmit}>
-        <Box width={"100%"}>
-          <InputGroup>
-            <input
-              type="text"
-              name="username"
-              style={{
-                background: "#EDEFF1",
-                height: "50px",
-                width: "300px",
-                borderRadius: "20px",
-                outline: "none",
-                padding: "0px 10px",
-                borderColor: `${
-                  errors.username && touched.username ? "red" : ""
-                }`,
-                borderWidth: "2px",
-              }}
-              placeholder="Username"
-              value={values.username}
-              onChange={handleChange}
-              onBlur={handleBlur}
-            />
-            {isValid && dirty && touched && (
-              <InputRightElement>
-                <CheckIcon color="green.500" />
-              </InputRightElement>
-            )}
-          </InputGroup>
-          {errors.username && touched.username && (
-            <Text
-              color="red"
-              fontSize={"12px"}
-              display={"flex"}
-              alignItems={"center"}
-            >
-              {errors.username}
-            </Text>
-          )}
-        </Box>
-        <Box width={"100%"} mt="15px">
-          <InputGroup>
-            <input
-              type="text"
-              name="email"
-              style={{
-                background: "#EDEFF1",
-                height: "50px",
-                width: "300px",
-                borderRadius: "20px",
-                outline: "none",
-                padding: "0px 10px",
-                borderColor: `${errors.email && touched.email ? "red" : ""}`,
-                borderWidth: "2px",
-              }}
-              placeholder="Email"
-              value={values.email}
-              onChange={handleChange}
-              onBlur={handleBlur}
-            />
-            {isValid && dirty && touched && (
-              <InputRightElement>
-                <CheckIcon color="green.500" />
-              </InputRightElement>
-            )}
-          </InputGroup>
-          {errors.email && touched.email && (
-            <Text
-              color="red"
-              fontSize={"12px"}
-              display={"flex"}
-              alignItems={"center"}
-            >
-              {errors.email}
-            </Text>
-          )}
-        </Box>
-        <Text fontSize={"13px"} mt="15px">
-          Forget your 
-        <span
-          style={{
-            textDecoration: "underline",
-            color: "#0079D3",
-            fontWeight: "bolder",
-              cursor: "pointer",
-            marginLeft: "5px"
-          }}
-          onClick={() =>
-            setAuthModalState((prev) => ({
-              ...prev,
-              View: "resetusername",
-            }))
-          }
-        >
-        Username
-          </span>
+      {success ? (
+        <Text>Check your mail </Text>
+      ) : (
+        <>
+          {" "}
+          <Text fontSize={"13px"} mb="20px">
+            Tell us the username and email address associated with your Reddit
+            account, and we’ll send you an email with a link to reset your
+            password.
           </Text>
-        <button
-          style={{
-            width: "100%",
-            background: "#ff4500",
-            opacity: `${isValid && dirty ? "1" : "0.3"}`,
-            marginTop: "15px",
-            height: "40px",
-            borderRadius: "20px",
-            color: "white",
-            fontSize: "14px",
-            fontWeight: "bold",
-          }}
-          disabled={!(isValid && dirty)}
-        >
-         Reset Password
-        </button>
-      </form>
+          <form onSubmit={handleSubmit}>
+            <Box width={"100%"}>
+              <InputGroup>
+                <input
+                  type="text"
+                  name="username"
+                  style={{
+                    background: "#EDEFF1",
+                    height: "50px",
+                    width: "300px",
+                    borderRadius: "20px",
+                    outline: "none",
+                    padding: "0px 10px",
+                    borderColor: `${
+                      errors.username && touched.username ? "red" : ""
+                    }`,
+                    borderWidth: "2px",
+                  }}
+                  placeholder="Username"
+                  value={values.username}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                {isValid && dirty && touched && (
+                  <InputRightElement>
+                    <CheckIcon color="green.500" />
+                  </InputRightElement>
+                )}
+              </InputGroup>
+              {errors.username && touched.username && (
+                <Text
+                  color="red"
+                  fontSize={"12px"}
+                  display={"flex"}
+                  alignItems={"center"}
+                >
+                  {errors.username}
+                </Text>
+              )}
+            </Box>
+            <Box width={"100%"} mt="15px">
+              <InputGroup>
+                <input
+                  type="text"
+                  name="email"
+                  style={{
+                    background: "#EDEFF1",
+                    height: "50px",
+                    width: "300px",
+                    borderRadius: "20px",
+                    outline: "none",
+                    padding: "0px 10px",
+                    borderColor: `${
+                      errors.email && touched.email ? "red" : ""
+                    }`,
+                    borderWidth: "2px",
+                  }}
+                  placeholder="Email"
+                  value={values.email}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                {isValid && dirty && touched && (
+                  <InputRightElement>
+                    <CheckIcon color="green.500" />
+                  </InputRightElement>
+                )}
+              </InputGroup>
+              {errors.email && touched.email && (
+                <Text
+                  color="red"
+                  fontSize={"12px"}
+                  display={"flex"}
+                  alignItems={"center"}
+                >
+                  {errors.email}
+                </Text>
+              )}
+            </Box>
+            <Text fontSize={"13px"} mt="15px">
+              Forget your
+              <span
+                style={{
+                  textDecoration: "underline",
+                  color: "#0079D3",
+                  fontWeight: "bolder",
+                  cursor: "pointer",
+                  marginLeft: "5px",
+                }}
+                onClick={() =>
+                  setAuthModalState((prev) => ({
+                    ...prev,
+                    View: "resetusername",
+                  }))
+                }
+              >
+                Username
+              </span>
+            </Text>
+            <Button
+              style={{
+                width: "100%",
+                background: "#ff4500",
+                opacity: `${isValid && dirty ? "1" : "0.3"}`,
+                marginTop: "15px",
+                height: "40px",
+                borderRadius: "20px",
+                color: "white",
+                fontSize: "14px",
+                fontWeight: "bold",
+              }}
+              isDisabled={!(isValid && dirty)}
+              onClick={onReset}
+              isLoading={sending}
+            >
+              Reset Password
+            </Button>
+          </form>
+        </>
+      )}
+
       <Text fontSize={"13px"}>
         Don't have an email or need assistance logging in? Get Help
       </Text>
